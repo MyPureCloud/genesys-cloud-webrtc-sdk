1fb3342aa4f45ae6895181e94d942bfdd91e3266
Xander Dumaine <xander.dumaine@inin.com>
2018-08-06 14:41:40 -0400

    Move webpack config

--- test/test-pages/build.sh
+++ test/test-pages/build.sh
@@ -2,14 +2,14 @@ pushd test/test-pages/
 echo "Building CDN sample..."
 cp index-template.html cdn/index.html
 cp -r common cdn/
-npx webpack --config webpack.config.js --entry ./cdn/common/main.js -o ./cdn/sdk-sample.js
+npx webpack --config webpack/webpack.config.js --entry ./cdn/common/main.js -o ./cdn/sdk-sample.js
 rm -rf cdn/common
 printf "CDN sample built successfully\n\n"
 
 echo "Building webpack sample..."
 cp -r common webpack/
 cp index-template.html webpack/index.html
-npx webpack --config webpack.config.js --entry ./webpack/common/main.js -o ./webpack/sdk-sample.js
+npx webpack --config webpack/webpack.config.js --entry ./webpack/common/main.js -o ./webpack/sdk-sample.js
 rm -rf webpack/common
 printf "Webpack sample built successfully\n\n"
 
