6f171248e9f3da89e08eead8cf765bee14a0f4fa
David Houseknecht <david.j.houseknecht@gmail.com>
2020-09-15 13:48:43 -0400

    PCM-1408 moved getScreenMedia request to start of screen share Because FF needs a userGesture to request the screen

--- test/unit/sessions/screen-share-session-handler.test.ts
+++ test/unit/sessions/screen-share-session-handler.test.ts
@@ -1,4 +1,4 @@
-import { SimpleMockSdk, MockSession, createPendingSession, MockStream } from '../../test-utils';
+import { SimpleMockSdk, MockSession, createPendingSession, MockStream, MockTrack } from '../../test-utils';
 import ScreenShareSessionHandler from '../../../src/sessions/screen-share-session-handler';
 import { GenesysCloudWebrtcSdk } from '../../../src/client';
 import { SessionManager } from '../../../src/sessions/session-manager';
@@ -31,19 +31,26 @@ describe('shouldHandleSessionByJid', () => {
 });
 
 describe('startSession', () => {
-  it('should initiate session', async () => {
-    const stream = new MockStream();
-    const jid = '123acdjid';
-    jest.spyOn(mediaUtils, 'startDisplayMedia').mockResolvedValue(stream as any);
-    jest.spyOn(utils, 'parseJwt').mockReturnValue({ data: { jid } });
-    const data = {
+  let stream: MockStream;
+  let jid: string;
+  let data: any;
+  beforeEach(() => {
+    /* setup necessary state data */
+    stream = new MockStream();
+    jid = '123acdjid';
+    data = {
       jwt: 'jwt',
       conversation: { id: 'conversationId1' },
       sourceCommunicationId: 'srcComId'
     };
-
     mockSdk._customerData = data;
 
+    /* spy on utility functions */
+    jest.spyOn(mediaUtils, 'startDisplayMedia').mockResolvedValue(stream as any);
+    jest.spyOn(utils, 'parseJwt').mockReturnValue({ data: { jid } });
+  });
+
+  it('should initiate session', async () => {
     await handler.startSession({ sessionType: SessionTypes.acdScreenShare });
 
     const expectedParams = {
@@ -55,6 +62,16 @@ describe('startSession', () => {
 
     expect(mockSdk._streamingConnection.webrtcSessions.initiateRtcSession)
       .toHaveBeenLastCalledWith(expectedParams);
+  });
+
+  it('should clear out any old media stream and set the new media', async () => {
+    const oldStream = new MockStream();
+    const endTracksSpy = jest.spyOn(handler, 'endTracks');
+
+    handler['_screenStreamPromise'] = Promise.resolve(oldStream) as any;
+    await handler.startSession({ sessionType: SessionTypes.acdScreenShare });
+
+    expect(endTracksSpy).toHaveBeenCalledWith(oldStream);
   });
 });
 
@@ -76,36 +93,64 @@ describe('handlePropose', () => {
 });
 
 describe('handleSessionInit', () => {
-  it('should end session if session init fails', async () => {
+  it('should end session and clean up any media if session init fails', async () => {
     const endSession = jest.spyOn(BaseSessionHandler.prototype, 'endSession').mockImplementation();
-    jest.spyOn(BaseSessionHandler.prototype, 'handleSessionInit').mockRejectedValue(new Error('error'));
-
+    const endTracks = jest.spyOn(handler, 'endTracks');
     const session: IJingleSession = new MockSession() as any;
-    await handler.handleSessionInit(session);
+    const stream = new MockStream();
 
-    expect(endSession).toHaveBeenCalled();
+    jest.spyOn(BaseSessionHandler.prototype, 'handleSessionInit').mockRejectedValue(new Error('error'));
+    handler['_screenStreamPromise'] = Promise.resolve(stream as any);
+
+    try {
+      await handler.handleSessionInit(session);
+      fail('should have thrown');
+    } catch (error) {
+      expect(endSession).toHaveBeenCalled();
+      expect(endTracks).toHaveBeenCalledWith(stream);
+      expect(handler['_screenStreamPromise']).toBe(null);
+    }
+  });
+
+  it('should throw if there is not a pending media screen promise', async () => {
+    jest.spyOn(BaseSessionHandler.prototype, 'endSession').mockImplementation();
+    jest.spyOn(BaseSessionHandler.prototype, 'handleSessionInit').mockImplementation();
+    const session: IJingleSession = new MockSession() as any;
+
+    try {
+      await handler.handleSessionInit(session);
+      fail('should have thrown');
+    } catch (error) {
+      expect(error.details.message).toBe('No pending or active screen share media promise');
+    }
   });
 
   it('should set a track listener that ends the session when all tracks have ended; should save stream to session', async () => {
+    jest.spyOn(BaseSessionHandler.prototype, 'handleSessionInit').mockImplementation();
     const acceptSpy = jest.spyOn(handler, 'acceptSession');
+
     const mockStream = (new MockStream() as any);
-    jest.spyOn(mediaUtils, 'startDisplayMedia').mockReturnValue(mockStream);
+    handler['_screenStreamPromise'] = Promise.resolve(mockStream);
     jest.spyOn(handler, 'addMediaToSession').mockImplementation();
 
     jest.spyOn((mockStream as MockStream)._tracks[0], 'addEventListener');
 
     const session: IJingleSession = new MockSession() as any;
-    await handler.handleSessionInit(session);
 
-    expect(session._screenShareStream).toBe(mockStream);
-    expect((mockStream as MockStream)._tracks[0].addEventListener).toHaveBeenCalled();
-    expect(acceptSpy).toHaveBeenCalled();
+    try {
+      await handler.handleSessionInit(session);
+    } catch (error) {
+      console.log(error.details);
+      expect(session._screenShareStream).toBe(mockStream);
+      expect((mockStream as MockStream)._tracks[0].addEventListener).toHaveBeenCalled();
+      expect(acceptSpy).toHaveBeenCalled();
+    }
   });
 
   it('should setup a terminated listener to stop _screenShareStream', async () => {
     const session: any = new MockSession();
     const stream = (new MockStream() as any);
-    jest.spyOn(mediaUtils, 'startDisplayMedia').mockReturnValue(stream)
+    handler['_screenStreamPromise'] = Promise.resolve(stream);
     jest.spyOn(handler, 'addMediaToSession').mockImplementation();
 
     session.emit.bind(session);
@@ -155,6 +200,23 @@ describe('onTrackEnd', () => {
 
     await handler.onTrackEnd(mockSession);
     expect(handler.endSession).toHaveBeenCalled();
+  });
+});
+
+describe('endTracks', () => {
+  it('should do nothing and not error if no stream was passed in', () => {
+    handler.endTracks();
+    expect('It did not thrown an error').toBeTruthy();
+  });
+
+  it('should end all tracks on a given stream', () => {
+    const track = new MockTrack('video');
+    const stream = new MockStream();
+    stream.addTrack(track);
+
+    handler.endTracks(stream as any);
+
+    expect(track.stop).toHaveBeenCalled();
   });
 });
 
