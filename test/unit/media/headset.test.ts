c1709a18e57a8166176b4f5d3d9139f2831df09f
Maxwell Mooney <maxwell.mooney@genesys.com>
2022-04-01 15:03:06 -0400

    PCM-1668 addressed some Codacy issues

--- test/unit/media/headset.test.ts
+++ test/unit/media/headset.test.ts
@@ -35,12 +35,12 @@ describe('SdkHeadset', () => {
     describe('updateAudioInputDevice', () => {
         it('should fetch the proper device and send it to the headset library', () => {
             const testId = "testId";
-            const findCachedDeviceByIdAndKindSpy = jest.spyOn(sdk.media, 'findCachedDeviceByIdAndKind' as any).mockReturnValue({
+            const findCachedDeviceByIdAndKindSpy = jest.spyOn(sdk.media, 'findCachedDeviceByIdAndKind').mockReturnValue({
                 kind: 'audioinput',
                 deviceId: 'testId',
                 label: 'Test Device Mark V',
             } as MediaDeviceInfo);
-            const activeMicChangeSpy = jest.spyOn(headsetLibrary, 'activeMicChange' as any);
+            const activeMicChangeSpy = jest.spyOn(headsetLibrary, 'activeMicChange');
             sdkHeadset.updateAudioInputDevice(testId);
             expect(findCachedDeviceByIdAndKindSpy).toHaveBeenCalledWith(testId, 'audioinput');
             expect(activeMicChangeSpy).toHaveBeenCalledWith('test device mark v');
@@ -73,14 +73,14 @@ describe('SdkHeadset', () => {
         it('should return false if disableRetry is false, isConnected is false but isConnecting is true', () => {
             headsetLibrary.activeMicChange('plantronics test device');
             headsetLibrary.selectedImplementation.isConnecting = true;
-            let showRetryResult = sdkHeadset.showRetry();
+            const showRetryResult = sdkHeadset.showRetry();
             expect(showRetryResult).toBe(false);
         })
 
         it('should return false if disableRetry is false, isConnecting is false but isConnected is true', () => {
             headsetLibrary.activeMicChange('plantronics test device');
             headsetLibrary.selectedImplementation.isConnected = true;
-            let showRetryResult = sdkHeadset.showRetry();
+            const showRetryResult = sdkHeadset.showRetry();
             expect(showRetryResult).toBe(false);
         })
 
