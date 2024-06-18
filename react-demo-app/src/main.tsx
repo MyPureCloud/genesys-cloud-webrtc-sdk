86e369aa4c0cd2e607a33bd0c1b9e61cce4a6094
Zach Servies <zachservies@gmail.com>
2024-06-18 15:13:07 -0400

    Install redux-toolkit. Create store.

--- react-demo-app/src/main.tsx
+++ react-demo-app/src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import 'normalize.css'; // CSS reset
+import { store } from './store.ts';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
