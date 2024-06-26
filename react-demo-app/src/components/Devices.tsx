c9b5a14899c700e56a86344c664d9e16797b21f5
Zach Servies <zachservies@gmail.com>
2024-06-26 16:57:50 -0400

    Devices dropdown cleanup.

--- react-demo-app/src/components/Devices.tsx
+++ react-demo-app/src/components/Devices.tsx
@@ -6,49 +6,94 @@ import useSdk from '../hooks/useSdk';
 export default function Devices() {
   const deviceState = useSelector((state) => state.devices);
   const { updateDefaultDevices } = useSdk();
+  const noDevices: JSX.Element = (
+    <p>No devices found.</p>
+  )
 
+  function renderAudioDevices() {
+    if (!deviceState.audioDevices.length) {
+      return noDevices;
+    }
+    const audioDeviceDropdown = (
+      <select
+        onChange={(e) =>
+          updateDefaultDevices({ audioDeviceId: e.target.value })
+        }
+      >
+        {deviceState.audioDevices.map((device) => (
+          <option key={device.deviceId} value={device.deviceId}>
+            {device.label}
+          </option>
+        ))}
+      </select>
+    );
+
+    return (
+      <Card className={undefined}>
+        <h3>Microphone Devices</h3>
+        { audioDeviceDropdown }
+      </Card>
+    );
+  }
+
+  function renderOutputDevices() {
+    if (!deviceState.outputDevices.length) {
+      return noDevices;
+    }
+    return (
+      <Card className={undefined}>
+        <h3>Output Devices</h3>
+        <select
+          onChange={(e) =>
+            updateDefaultDevices({ outputDeviceId: e.target.value })
+          }
+        >
+          {deviceState.outputDevices.map((device) => (
+            <option key={device.deviceId} value={device.deviceId}>
+              {device.label}
+            </option>
+          ))}
+        </select>
+      </Card>
+    );
+  }
+
+  function renderVideoDevices() {
+    if (!deviceState.videoDevices.length) {
+      return noDevices;
+    }
+    return (
+      <Card className={undefined}>
+        <h3>Video Devices</h3>
+        <select
+          onChange={(e) =>
+            updateDefaultDevices({ videoDeviceId: e.target.value })
+          }
+        >
+          {deviceState.videoDevices.map((device) => (
+            <option key={device.deviceId} value={device.deviceId}>
+              {device.label}
+            </option>
+          ))}
+        </select>
+      </Card>
+    );
+  }
   return (
     <>
       <h1>Devices</h1>
       <div className="devices-container">
         <Card className={undefined}>
-          <h3>Microphone Devices</h3>
-          <select
-            name="audio-devices"
-            onChange={(e) =>
-              updateDefaultDevices({ audioDeviceId: e.target.value })
-            }
-          >
-            {deviceState.audioDevices.map((device) => (
-              <option value={device.deviceId}>{device.label}</option>
-            ))}
-          </select>
+          <h3>Audio Devices</h3>
+          {renderAudioDevices()}
         </Card>
         <Card className={undefined}>
           <h3>Output Devices</h3>
-          <select
-            name="output-devices"
-            onChange={(e) =>
-              updateDefaultDevices({ outputDeviceId: e.target.value })
-            }
-          >
-            {deviceState.outputDevices.map((device) => (
-              <option value={device.deviceId}>{device.label}</option>
-            ))}
-          </select>
+          {renderOutputDevices()}
         </Card>
         <Card className={undefined}>
           <h3>Video Devices</h3>
-          <select
-            name="video-devices"
-            onChange={(e) =>
-              updateDefaultDevices({ videoDeviceId: e.target.value })
-            }
-          >
-            {deviceState.videoDevices.map((device) => (
-              <option value={device.deviceId}>{device.label}</option>
-            ))}
-          </select>
+          {renderVideoDevices()}
         </Card>
       </div>
     </>
