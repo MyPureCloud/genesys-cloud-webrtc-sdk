9f8d671d6886659ec376b2245b6ecb2392317d0a
Zach Servies <zachservies@gmail.com>
2024-06-28 00:44:13 -0400

    Router leftovers.

--- react-demo-app/src/main.tsx
+++ react-demo-app/src/main.tsx
@@ -5,14 +5,11 @@ import './index.css'
 import 'normalize.css'; // CSS reset
 import { store } from './store.ts';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>,
 )
