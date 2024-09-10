05a28a143cd93356cf049d61a5f2f6c2b399de2b
Jon Hjelle <jon.hjelle@genesys.com>
2024-09-10 16:15:56 -0700

    [STREAM-162] Proposal for handling invalid tokens

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from 'events';
 import StrictEventEmitter from 'strict-event-emitter-types';
-import StreamingClient, { HttpClient } from 'genesys-cloud-streaming-client';
+import StreamingClient, { HttpClient, InvalidTokenError } from 'genesys-cloud-streaming-client';
 import Logger from 'genesys-cloud-client-logger';
 import { jwtDecode } from "jwt-decode";
 
@@ -287,6 +287,10 @@ export class GenesysCloudWebrtcSdk extends (EventEmitter as { new(): StrictEvent
 
       this.emit('ready');
     } catch (err) {
+      if (err instanceof InvalidTokenError) {
+        throw createAndEmitSdkError.call(this, SdkErrorTypes.invalid_token, err.message, err);
+      }
+
       throw createAndEmitSdkError.call(this, SdkErrorTypes.initialization, err.message, err);
     }
   }
