b169afd79db6b1f7e444dc53a62a0721a3176046
Jon Hjelle <jon.hjelle@genesys.com>
2024-09-19 17:00:15 -0700

    [STREAM-162] Update InvalidTokenError -> StreamingClientError

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from 'events';
 import StrictEventEmitter from 'strict-event-emitter-types';
-import StreamingClient, { HttpClient, InvalidTokenError } from 'genesys-cloud-streaming-client';
+import StreamingClient, { HttpClient, StreamingClientError, StreamingClientErrorTypes } from 'genesys-cloud-streaming-client';
 import Logger from 'genesys-cloud-client-logger';
 import { jwtDecode } from "jwt-decode";
 
@@ -287,7 +287,7 @@ export class GenesysCloudWebrtcSdk extends (EventEmitter as { new(): StrictEvent
 
       this.emit('ready');
     } catch (err) {
-      if (err instanceof InvalidTokenError) {
+      if (err instanceof StreamingClientError && err.type == StreamingClientErrorTypes.invalid_token) {
         throw createAndEmitSdkError.call(this, SdkErrorTypes.invalid_token, err.message, err);
       }
 
