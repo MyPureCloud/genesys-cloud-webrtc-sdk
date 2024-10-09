a085dbd6508c94f29def347383c46dabe440ef59
Zach Servies <zachservies@gmail.com>
2024-10-09 14:46:46 -0400

    Cleanup ternaries.

--- react-demo-app/src/components/AudioDevices.tsx
+++ react-demo-app/src/components/AudioDevices.tsx
@@ -18,44 +18,48 @@ export default function AudioDevices() {
     setAudioVolume(volume);
   }
 
+  function displayAudioDevices() {
+    if (deviceState.audioDevices.length) {
+      return <>
+        <div className="audio-device-list">
+          <GuxDropdown
+            value={deviceState.audioDevices[0].deviceId}
+            onInput={(e) =>
+              updateDefaultDevices({ audioDeviceId: e.target.value })
+            }
+          >
+            <GuxListbox>
+              {deviceState.audioDevices.map((device) => (
+                <GuxOption key={device.deviceId} value={device.deviceId}>
+                  {device.label}
+                </GuxOption>
+              ))}
+            </GuxListbox>
+          </GuxDropdown>
+        </div>
+        <div className="audio-device-volume">
+          <label htmlFor="audio-volume">Audio Volume</label>
+          <input
+            name="audio-volume"
+            type="range"
+            min="0"
+            max="100"
+            value={audioVolume}
+            onChange={(e) => updateVolume(e.target.value)}
+          />
+          <span className="audio-volume-tooltip">{audioVolume}%</span>
+        </div>
+      </>
+    } else {
+      return <p>No audio devices.</p>
+    }
+  }
+
   return (
     <>
       <Card className='audio-devices-container'>
         <h4>Audio Devices</h4>
-        {deviceState.audioDevices.length ? (
-          <>
-            <div className="audio-device-list">
-              <GuxDropdown
-                value={deviceState.audioDevices[0].deviceId}
-                onInput={(e) =>
-                  updateDefaultDevices({ audioDeviceId: e.target.value })
-                }
-              >
-                <GuxListbox>
-                  {deviceState.audioDevices.map((device) => (
-                    <GuxOption key={device.deviceId} value={device.deviceId}>
-                      {device.label}
-                    </GuxOption>
-                  ))}
-                </GuxListbox>
-              </GuxDropdown>
-            </div>
-            <div className="audio-device-volume">
-              <label htmlFor="audio-volume">Audio Volume</label>
-              <input
-                name="audio-volume"
-                type="range"
-                min="0"
-                max="100"
-                value={audioVolume}
-                onChange={(e) => updateVolume(e.target.value)}
-              />
-              <span className="audio-volume-tooltip">{audioVolume}%</span>
-            </div>
-          </>
-        ) : (
-          <p>No audio devices.</p>
-        )}
+        {displayAudioDevices()}
       </Card>
     </>
   );
--- react-demo-app/src/components/VideoDevices.tsx
+++ react-demo-app/src/components/VideoDevices.tsx
@@ -7,28 +7,32 @@ export default function OutputDevices() {
   const { updateDefaultDevices } = useSdk();
   const deviceState = useSelector((state) => state.devices.currentState);
 
+  function generateVideoDevices() {
+    if (deviceState.videoDevices.length) {
+      return (
+        <GuxDropdown
+          value={deviceState.videoDevices[0].deviceId}
+          onInput={(e) =>
+            updateDefaultDevices({ videoDeviceId: e.target.value })
+          }
+        >
+          <GuxListbox>
+            {deviceState.videoDevices.map((device) => (
+              <GuxOption key={device.deviceId} value={device.deviceId}>
+                {device.label}
+              </GuxOption>
+            ))}
+          </GuxListbox>
+        </GuxDropdown>);
+    } else {
+      return <p>No video devices.</p>;
+    }
+  }
   return (
     <>
-      <Card className="video-devices-contaner">
+      <Card className="video-devices-container">
         <h4>Video Devices</h4>
-        {deviceState.videoDevices.length ? (
-          <GuxDropdown
-            value={deviceState.videoDevices[0].deviceId}
-            onInput={(e) =>
-              updateDefaultDevices({ videoDeviceId: e.target.value })
-            }
-          >
-            <GuxListbox>
-              {deviceState.videoDevices.map((device) => (
-                <GuxOption key={device.deviceId} value={device.deviceId}>
-                  {device.label}
-                </GuxOption>
-              ))}
-            </GuxListbox>
-          </GuxDropdown>
-        ) : (
-          <p>No video devices.</p>
-        )}
+        {generateVideoDevices()}
       </Card>
     </>
   );
