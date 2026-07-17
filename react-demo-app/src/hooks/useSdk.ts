92e4135fc82496662d14d2ed38f22b4d1ab827df
bruegg <40368923+bruegg@users.noreply.github.com>
2026-07-17 15:59:29 -0400

    STREAM-1968 Make new noise tester for the demo app to use with Iris c… (#1038)
    Co-authored-by: BrueggyBot <c-media-gc-brueggybot-auth@genesys.com>

--- react-demo-app/src/hooks/useSdk.ts
+++ react-demo-app/src/hooks/useSdk.ts
@@ -26,8 +26,8 @@ import { MinimalSdk } from '../types/sdk';
 // The SDK accepts any audio processor that implements the IAudioProcessor interface.
 // GenesysIrisClient is used here as a concrete example implementation.
 // Manually add these in either the src or public folder of the demo app.
-import licenseText from '../../iris.ilc?raw';
-import keyText from '../../iris.key?raw';
+import licenseText from '../assets/iris.lic?raw';
+import keyText from '../assets/iris.key?raw';
 import { GenesysIrisClient } from 'genesys-iris-client';
 import { setAudioProcessor } from '../features/audioProcessorSlice';
 import { IAudioProcessor } from 'genesys-cloud-webrtc-sdk';
